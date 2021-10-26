471c7acf07562bf94b5f7ca53ce23121079ab8fa
Serjge <serjge@yandex.ru>
2021-10-26 19:58:14 +0300

    listened to 3 videos OOP

--- oop it_kamasutra/app.js
+++ oop it_kamasutra/app.js
@@ -1,15 +1,34 @@
 function startCarListener() {
-	let randomNumber = Math.random()
-	if (randomNumber > 0.5) {
-		alert("Car have started")
-		function engineCrash() {
-			alert('Engine Crashed')
-		}
-		setTimeout(engineCrash, 5000)
-	} else {
-		alert("Something wrong")
-	}
+  let randomNumber = Math.random();
+  if (randomNumber > 0.5) {
+    carStarted();
+  } else {
+    carCannotBeStarted();
+  }
 }
 
-const startButton = document.querySelector('#start_car')
-startButton.addEventListener('click', startCarListener)
+function drawStatus(status) {
+  statusLabel.innerHTML = status;
+}
+function carStarted() {
+  drawStatus('Car have started');
+  console.log('Car have started');
+
+  startButton.classList.add('hide');
+
+  function engineCrash() {
+    drawStatus('Engine Crashed');
+    console.log('Engine Crashed');
+    startButton.classList.remove('hide');
+  }
+  setTimeout(engineCrash, 5000);
+}
+
+function carCannotBeStarted() {
+  drawStatus("Car can't be started");
+  console.log("Car can't be started");
+}
+
+const startButton = document.querySelector('#start_car');
+const statusLabel = document.querySelector('#status');
+startButton.addEventListener('click', startCarListener);
