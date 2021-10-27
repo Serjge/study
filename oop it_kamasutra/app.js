70f74e3fe228857ec961969d3089be1ce54d3df7
Sergey Dudkin <Serjge@yandex.ru>
2021-10-27 15:16:52 +0300

    viewed 4 videos

--- oop it_kamasutra/app.js
+++ oop it_kamasutra/app.js
@@ -1,34 +1,60 @@
 function startCarListener() {
-  let randomNumber = Math.random();
-  if (randomNumber > 0.5) {
-    carStarted();
-  } else {
-    carCannotBeStarted();
-  }
+	let randomNumber = Math.random();
+	if (randomNumber > 0.5) {
+		carStarted();
+	} else {
+		carCannotBeStarted();
+	}
 }
 
 function drawStatus(status) {
-  statusLabel.innerHTML = status;
+	statusLabel.innerHTML = status;
 }
 function carStarted() {
-  drawStatus('Car have started');
-  console.log('Car have started');
+	drawStatus('Car have started');
 
-  startButton.classList.add('hide');
+	startButton.classList.add('hide');
 
-  function engineCrash() {
-    drawStatus('Engine Crashed');
-    console.log('Engine Crashed');
-    startButton.classList.remove('hide');
-  }
-  setTimeout(engineCrash, 5000);
+	gearBoxStart()
+	plannedCrashStarted()
+
 }
 
 function carCannotBeStarted() {
-  drawStatus("Car can't be started");
-  console.log("Car can't be started");
+	drawStatus("Car can't be started");
+
+}
+
+function gearBoxStart() {
+	let gearBoxValue = 1
+
+	gearBoxValueabel.innerHTML = gearBoxValue
+	function incrementGearBoxValue() {
+		if (gearBoxValue < 5) {
+			gearBoxValue++
+			gearBoxValueabel.innerHTML = gearBoxValue
+		}
+	}
+
+	gearBoxInterval = setInterval(incrementGearBoxValue, 1000);
+
+}
+
+function plannedCrashStarted() {
+	function engineCrash() {
+		drawStatus('Engine Crashed');
+		console.log('Engine Crashed');
+		startButton.classList.remove('hide');
+
+		gearBoxValueabel.innerHTML = 'N'
+		clearInterval(gearBoxInterval)
+	}
+	setTimeout(engineCrash, 3000);
 }
 
 const startButton = document.querySelector('#start_car');
 const statusLabel = document.querySelector('#status');
+const gearBoxValueabel = document.querySelector('#gearBoxValue');
 startButton.addEventListener('click', startCarListener);
+
+let gearBoxInterval
