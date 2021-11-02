85f49e174c7deabcffb09930af9d33b6f6648917
Serjge <serjge@yandex.ru>
2021-11-02 19:32:28 +0300

    react v 0.1

---
+++ react/01/react-1/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+import Header from './Header.js';
+import Technologies from './Technologies';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <Technologies />
+    </div>
+  );
+};
+
+export default App;
